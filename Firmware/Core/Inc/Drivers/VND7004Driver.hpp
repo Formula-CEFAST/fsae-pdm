51b7e00123bff881a66b26f4688a7dc21c48c05d
Davi Marriel <davimarriel@gmail.com>
2024-03-12 18:31:40 -0300

    Creating driver files

---
+++ Firmware/Core/Inc/Drivers/VND7004Driver.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace drivers {
+class VND7004 {
+   private:
+    /* data */
+   public:
+    VND7004(/* args */);
+    ~VND7004();
+};
+
+}  // namespace drivers
---
+++ Firmware/Core/Inc/Drivers/VNQ7040Driver.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace drivers {
+class VNQ7040 {
+   private:
+    /* data */
+   public:
+    VNQ7040(/* args */);
+    ~VNQ7040();
+};
+}  // namespace drivers
